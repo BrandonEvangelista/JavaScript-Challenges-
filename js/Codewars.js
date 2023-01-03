69c443baac87bb10d259c872b876babc919b0eb1
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-02 23:54:23 -0800

    Challenge 1/2/23
    Create a function that detects if first letter is the letter "r"
    -takes string, creates array, checks if interval 0 is the letter R

--- js/Codewars.js
+++ js/Codewars.js
@@ -1,1 +1,29 @@
 console.log("these nuts")
+
+/*
+/1/2/2023
+
+C/reate a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings
+
+*/
+
+// THis is the solution 
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  }
+  
+
+console.log (areYouPlayingBanjo("dan")) 
+
+console.log (areYouPlayingBanjo("rex")) 
+
+console.log (areYouPlayingBanjo("ron")) 
+
+console.log (areYouPlayingBanjo("paul")) 
