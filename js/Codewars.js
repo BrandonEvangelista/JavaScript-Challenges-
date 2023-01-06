0261bc5fe7b02d21ce43edd9648c06d0c947b20b
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-05 20:22:39 -0800

    Used conditional to solve fuel economy
    Use of if statements with conjunction of the OR  sign. can help determine if a solution is valid.

--- js/Codewars.js
+++ js/Codewars.js
@@ -59,3 +59,30 @@ function makeUpperCase(str) {
 }
 let a = "testingthefunction"
 console.log(makeUpperCase(a))
+
+
+/* You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not.
+*/
+
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+  
+  if ((distanceToPump < (mpg*fuelLeft)) || (distanceToPump == (mpg*fuelLeft))){
+    console.log ("rainbow friends forver! long live the king, we made it home")
+  } else {
+    console.log ("fin")
+  }
+};
+
+
+let distToCheveron = 1000
+let tank = 9000
+let fordMpg= .3
+
+console.log (zeroFuel(distToCheveron,fordMpg,tank))
