c6b51c79aef1f75cb75dab21d5d6c944e2ba3621
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-07 21:40:44 -0800

    worked on conditionals. used if then statements

--- js/Codewars.js
+++ js/Codewars.js
@@ -100,3 +100,16 @@ console.log (zeroFuel(distToCheveron,fordMpg,tank))
 
 getEvenNumbers(randomnum)
 console.log(getEvenNumbers(randomnum))
+
+
+/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+
+function boolToWord( bool ){
+  //...
+  
+  if (bool == true){
+    return "Yes" }
+  else {
+    return "No"
+  }
+}
