81f625d8ce5cb9dce04642c662426da0441da590
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-08 23:30:44 -0800

    Use conditionals

--- js/Codewars.js
+++ js/Codewars.js
@@ -166,3 +166,20 @@ function getCount(str) {
     return count
     
   }
+
+  /* Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+*/
+function lovefunc(a, b){
+  // moment of truth
+  let c = a%2
+  let d = b%2
+  console.log(c)
+  console.log(d)
+  
+  if ((c > d) ||  (c < d) ){
+    return true
+  } else {return false}  
+}
