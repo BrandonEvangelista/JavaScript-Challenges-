09d711646318238d32338a35dc0933cc408dfd65
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-14 23:48:25 -0800

    longest substring

--- js/Codewars.js
+++ js/Codewars.js
@@ -329,6 +329,33 @@ function reverseWords(str) {
    final.push( revphrase[i].split('').reverse().join(''));
 }
   
+
+/* longest substring */
+
+
+const solution = (s) => {
+  if (s.length === 0) return 0
+      
+      
+   let set = new Set() 
+   let i =0 
+   let j = 0 
+   let maxLength = 0
+   
+   while (j < s.length){
+   if (!set.has (s[j]))  {
+       set.add(s[j])
+       j++
+  } else {
+   set.delete(s[i])
+  i++
+  }
+       maxLength = Math.max(maxLength,set.size);
+   }
+     return maxLength
+  
+  
+  };
   
   return final.join(" ")
   
