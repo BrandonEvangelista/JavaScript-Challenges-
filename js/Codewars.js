1928b01160b4b5ac73e3fed6e13eea666c6da6e2
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-16 22:51:43 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -406,4 +406,43 @@ function booleanToString(b){
   return b ? 'true' : 'false';
 }
 
+/* Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA" */  
+
+function DNAStrand(dna){
+  //your code here
+  let newword = []
+  let spread = dna.split('')
+  
+  function DNAStrand(dna){
+    //your code here
+    let newword = []
+    let spread = dna.split('')
+    
+    for ( let i=0;i<spread.length;i++ ){
+      if (spread[i]=="T"){
+        newword.push('A')
+      }
+      else if (spread[i]=="A"){
+        newword.push('T')
+      }
+      else if (spread[i]=="G"){
+        newword.push('C')
+      }
+      else if (spread[i]=="C"){
+        newword.push('G')
+      }
+    }
+    console.log (newword)
+    return newword.join('')
+  }
