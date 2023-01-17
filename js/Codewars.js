0448926a7c8fe86f270c16b554c97ea3e7cdccaa
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-17 07:56:52 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -468,3 +468,48 @@ for (let i = 0 ; i< n ; i++)
   }
   return car.join("")
 }
+ 
+/* The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0  */
+
+function checkExam(array1, array2) {
+  // 4 points for each correct
+ // -1 point for each incorrect answer
+ //. 0 points for each blank ""
+   
+   let score = 0
+   for (let i = 0; i < array1.length ; i++){
+     
+     if (array2[i]==""){
+       console.log('blanky')
+       score += 0
+     }
+     
+     else if (array1[i] == array2[i]){
+       console.log ('4 points for Griffindor')
+       score += 4
+     }
+     
+     else if (array1[i] !== array2 [i]){
+       console.log('404 error')
+       score += -1
+     }
+     
+     
+   }
+   console.log (score)
+   if (score < 0){return 0}
+   
+   if (score >= 0 ){return score}
+     
+ }
