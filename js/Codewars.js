7c271ce37f7901c08dd8aaf3af9c2a4f77d8add6
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-01-26 21:54:23 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -924,4 +924,20 @@ function Stock (name, code, quantity, price) {
              new ("Caramac Bar", "A02", 5, 0.60)
              new ("Dairy Milk","A03", 1, 0.65)
              new ("Freddo","A04", 1, 0.25)
+asdfasfasdf   var    fred   =   new Stock("Freddo","A04", 1, 0.25)
 asdfasfasdf
+
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str){
+  let cat = str.split(' ')
+  
+  for (let i = 1 ; i < cat.length ;i+=2)
+    {
+      cat[i]=cat[i].split('').reverse().join('').trim()
+    }
+  console.log (cat)
+  return cat.join(' ').trim()
+}
