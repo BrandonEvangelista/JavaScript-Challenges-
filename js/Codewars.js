52efe8a6519be9a6e17e6a33b11bd235835741ee
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-04 20:25:39 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -983,4 +983,12 @@ function duplicateCount(text){
   
   return result;
   
-}//sadfg
+}
+
+
+//Complete the solution so that it reverses the string passed into it.
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+
