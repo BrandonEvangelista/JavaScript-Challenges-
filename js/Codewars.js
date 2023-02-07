a144549a0a6ebecbcb1dfcfbd7df60a486cec1ff
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-06 21:53:49 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1137,3 +1137,14 @@ const rps = (p1, p2) => {
     return winner1
   }
 };
+
+const rps2 = (p1, p2) => {
+  if (p1===p2){
+    return ("Draw!")
+    }
+  var rules = {rock: "scissors", scissors: "paper", paper: "rock"};
+     if (p2 === rules[p1]){
+    return ("Player 1 won!");
+  }
+    else {return ("Player 2 won!");}
+  }
