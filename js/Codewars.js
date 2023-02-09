d3b4d7442fe47aee1fef60a805038874d22b91ca
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-08 23:51:30 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1217,4 +1217,16 @@ function chessBoard(rows, columns) {
       
       return board
   }
-  //asdfghjk
+  //It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+  function removeChar(str){
+    //You got this!
+     let spread = str.split('')
+     let len = spread.length
+     
+     spread.pop();
+     spread.shift();
+     return spread.join('')
+   
+   };
+   
