1ad7d6ca15146b1130b1c8f60bb3d275f7fb0717
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-09 19:30:12 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1239,5 +1239,34 @@ function chessBoard(rows, columns) {
     }
    return empty.join('')
    };
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Note: If the number is a multiple of both 3 and 5, only count it once.*/
+function solution(number){
 
+for(let i=0; i<number; i--){
+
+}
+}
+
+/*  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Note: If the number is a multiple of both 3 and 5, only count it once.*/
+function solution(number){
+  if (number<=0){
+    return 0
+  }
+  let sum = 0
+  for (let i = 0; i<number; i++){
+    if (i%5==0)  {
+      sum = sum + i
    
+    }
+    else if (i%3==0) {
+      sum = sum + i
+     
+    }
+   
+  }
+    return sum
+}
