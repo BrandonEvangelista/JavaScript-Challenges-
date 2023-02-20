b50a323d11c4f0233a4eb4bea730d8f5371361f0
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-19 21:44:15 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1326,3 +1326,21 @@ function noSpace(x){
   }
   return sum
 }
+
+function findOdd(arr) {
+  var result, num = 0;
+
+  arr = arr.sort();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i+1]) {
+      num++;
+    } else {
+      num++;
+      if (num % 2 != 0) {
+        result = arr[i];
+        break;
+      }
+    }
+  }
+  return result;
+}
