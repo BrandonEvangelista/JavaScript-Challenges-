4c08c6b480a08c8bb713388d6446e602576c8ee5
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-02-23 22:39:36 -0800

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1392,3 +1392,6 @@ function findAverage(array) {
 
 //  You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
 
+function check(a, x) {
+  return (a.includes(x))
+  }
