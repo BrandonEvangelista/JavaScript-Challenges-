be57c7802f279c34411ef2dba692d4c5bcf370db
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-07-20 23:23:39 -0700

    Update Codewars.js

--- js/Codewars.js
+++ js/Codewars.js
@@ -1552,3 +1552,52 @@ const areaOrPerimeter = function(l , w) {
     return ((2*l)+(2*w))
   }
 };
+
+// Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+
+// A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+
+// Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+
+// your referral bonus, and
+
+// the price of a beer can
+
+// For example:
+
+// beeramid(1500, 2); // should === 12
+// beeramid(5000, 3); // should === 16
+
+
+// Returns number of complete beeramid levels
+
+// levels 1 = 1
+// level 2 = 4
+//level 3 =9
+// you are given total amount of money = bonus
+// price per can i given
+
+// bonus = number of cans * price
+// number of cans = number of levels / factorial 
+var beeramid = function(bonus, price) {
+  let levelCount = 0 ;
+  let currentCanCount = 0
+  let totalCans = 0
+  let grandTotal = 0;
+  const perCan = price;
+  if (bonus <= 0){
+    return 0
+  }
+
+  while (bonus >= grandTotal) {
+    levelCount ++;  // this increments the Level counter
+    currentCanCount = Math.pow(levelCount,2); // this counts the cans at the current level 
+    totalCans = currentCanCount + totalCans; // this adds the cans to total can count 
+
+    grandTotal = grandTotal + (currentCanCount*perCan);
+    
+    
+}
+return levelCount-1
+
+}
